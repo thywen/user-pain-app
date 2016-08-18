8b56ea4d1ecdd66210e8033dd8a73e798ded361e
Sven Kroll <Sven.Kroll@bbc.com>
2016-08-18 13:53:23 +0100

    add tests for show action

--- spec/controllers/bugs_controller_spec.rb
+++ spec/controllers/bugs_controller_spec.rb
@@ -22,4 +22,29 @@ RSpec.describe BugsController, type: :controller do
       it { is_expected.to be_success }
     end
   end
+
+  describe '#show' do
+    context 'bug existing' do
+      let(:bug) { bugs[rand number_of_bugs] }
+      before(:each) { get :show, id: bug.id }
+
+      describe 'success' do
+        subject { response }
+
+        it { is_expected.to be_success }
+      end
+
+      describe 'assign bug' do
+        subject { assigns(:bug) }
+
+        it { is_expected.to eq bug }
+      end
+    end
+
+    context 'bug non existing' do
+      it 'throws ActiveRecord::RecordNotFound' do
+        expect { get :show, id: -1 }.to raise_exception ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
